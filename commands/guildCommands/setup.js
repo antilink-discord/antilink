53bbd373bbf67827065901b24de355d6dbf0700c
Максим <60219618+FelladoR@users.noreply.github.com>
2025-01-27 23:37:13 +0200

    updated setup.js

--- commands/guildCommands/setup.js
+++ commands/guildCommands/setup.js
@@ -2,7 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder, MessageFlags, Message, ChannelType } = require('discord.js');
 require("moment-duration-format");
 const Guild = require('../../Schemas/guildSchema')
-const { clear_guild_language_cache } = require('../../utils/helper')
+const { clear_guild_language_cache, getTranslation, colors } = require('../../utils/helper');
+const { get } = require('mongoose');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('setup')
@@ -60,12 +62,9 @@ module.exports = {
                     )),
         ),
             
-
-            
-        
-    // Визначення execute з параметром client
     async execute(interaction) {
         if (interaction.options.getSubcommand() === 'log_channel') {
+            try{
             const guildData = await Guild.findOne({ _id: interaction.guild.id})
             const url = interaction.options.getString('webhook_url')
             console.log(guildData)
@@ -75,41 +74,59 @@ module.exports = {
                     { _id: interaction.guild.id},
                     { $set: {logchannel: url}}
                 )
-                await interaction.reply(`Webhook для логів успішно призначено!`)
+            if(url == guildData.logchannel) {
+                await interaction.reply({ content: await getTranslation(interaction.guild.id, "setup_logchannel_webhoook_isthesame"), flags: MessageFlags.Ephemeral})
+                return
             }
+                const SuccessfullEmbed = new EmbedBuilder()
+                    .setColor(0xAEFFD8)
+                    .setThumbnail(interaction.guild.iconURL({dynamic: true, size: 1024}))
+                    .setTitle(await getTranslation(interaction.guild.id, "setup_successful"))
+                    .setDescription(await getTranslation(interaction.guild.id, "setup_logchannel_changed"))
+                await interaction.reply({ embeds: [SuccessfullEmbed], flags: MessageFlags.Ephemeral})
+            }
+        }catch(error) {
+            await interaction.reply(await getTranslation(interaction.guild.id, "main_error_message"))
+            return
+        }
             
         }
         if (interaction.options.getSubcommand() === 'whitelist') {
             try{
                 const role = interaction.options.getRole('role')
-                //console.log(role)
+
                 let guildData = await Guild.findOne({ _id: interaction.guild.id} )
                 console.log(guildData)
                 if(!guildData) {
-                    console.log('Не знайдено гільдію, створюю запис')
                     guildData = new Guild({ _id: interaction.guild.id})
                     await guildData.save()
                 }
                 if (!guildData.whitelist.includes(role.id)) {
-                    guildData.whitelist.push(role.id); // Додаємо ID ролі до масиву
-                    await guildData.save(); // Зберігаємо зміни в базу даних
-                    await interaction.reply(`Роль успішно додано до whitelist: ${role}`);
+
+                    guildData.whitelist.push(role.id); 
+                    await guildData.save(); 
+                    const SuccessfullEmbed = new EmbedBuilder()
+                        .setColor(color.SUCCESSFUL_COLOR)
+                        .setThumbnail(interaction.guild.iconURL({dynamic: true, size: 1024}))
+                        .setTitle(await getTranslation(interaction.guild.id, "setup_successful"))
+                        .setDescription(await getTranslation(interaction.guild.id, "setup_whitelist_changed", {role}))
+                    await interaction.reply({ embeds: [SuccessfullEmbed], flags: MessageFlags.Ephemeral})
                 } else {
-                    await interaction.reply(`Роль вже є у whitelist: ${role}`);
+                    await interaction.reply(await getTranslation(interaction.guild.id, "setup_whitelist_already_is"));
                 }
             }catch(error) {
-                console.log(error)
+                await interaction.reply(await getTranslation(interaction.guild.id, "main_error_message"))
+                return
             }
         }
         if (interaction.options.getSubcommand() === 'language') {
+
             try{
-                console.log('Команда викликається')
                 const lang = interaction.options.getString('set_language_option')
-                console.log(`Вибрана мова: `+lang)
                 let guildData = await Guild.findOne({ _id: interaction.guild.id} )
                 console.log(guildData)
+
                 if(!guildData) {
-                    console.log('Не знайдено гільдію, створюю запис')
                     guildData = new Guild({ _id: interaction.guild.id})
                     await guildData.save()
                 }
@@ -118,29 +135,64 @@ module.exports = {
                     { $set: {language: lang}}
                 )
                 clear_guild_language_cache(interaction.guild.id)
-                await interaction.reply('Успішно змінено мову на ' + lang)
+                const SuccessfullEmbed = new EmbedBuilder()
+                        .setColor(color.SUCCESSFUL_COLOR)
+                        .setThumbnail(interaction.guild.iconURL({dynamic: true, size: 1024}))
+                        .setTitle(await getTranslation(interaction.guild.id, "setup_successful"))
+                        .setDescription(await getTranslation(interaction.guild.id, "setup_language_changed", {lang}))
+                    await interaction.reply({ embeds: [SuccessfullEmbed], flags: MessageFlags.Ephemeral})
+
+            }catch(error) {
+                await interaction.reply(await getTranslation(interaction.guild.id, "main_error_message"))
+                return
+            }
+        }
+
+        if (interaction.options.getSubcommand() === 'ban_users') {
+            try{
+                const choice = interaction.options.getString('ban_users_option')
+                const guildData = await Guild.findOne({ _id: interaction.guild.id})
+                const isChoiceTrue = choice === 'true'
+
+                if(guildData.blocking_enabled === isChoiceTrue) { // Перевірка, чи було введено той самий параметр, який вже встановлений на сервері
+                    await interaction.reply(await getTranslation(interaction.guild.id, "setup_banusers_isthesame"))
+                    return
+                }
+                if(choice==='true') {
+                    try{
+                        await Guild.updateOne(
+                            { _id: interaction.guild.id },
+                            { $set: { blocking_enabled: true } }
+                        );
+                        const SuccessfullEmbed = new EmbedBuilder()
+                            .setColor(colors.SUCCESSFUL_COLOR)
+                            .setThumbnail(interaction.guild.iconURL({dynamic: true, size: 1024}))
+                            .setTitle(await getTranslation(interaction.guild.id, "setup_successful"))
+                            .setDescription(await getTranslation(interaction.guild.id, "setup_banusers_enabled"))
+                        await interaction.reply({ embeds: [SuccessfullEmbed], flags: MessageFlags.Ephemeral})
+                    }catch(error) {
+                        console.log(error)
+                    }
+                }else if(choice==='false') {
+                    try{
+                        await Guild.updateOne(
+                            { _id: interaction.guild.id },
+                            { $set: { blocking_enabled: false } }
+                        );
+                        const SuccessfullEmbed = new EmbedBuilder()
+                            .setColor(colors.SUCCESSFUL_COLOR)
+                            .setThumbnail(interaction.guild.iconURL({dynamic: true, size: 1024}))
+                            .setTitle(await getTranslation(interaction.guild.id, "setup_successful"))
+                            .setDescription(await getTranslation(interaction.guild.id, "setup_banusers_disabled"))
+                        await interaction.reply({ embeds: [SuccessfullEmbed], flags: MessageFlags.Ephemeral})
+                    }catch(error) {
+                        console.log(error)
+                    }
+                }
+                
             }catch(error) {
                 console.log(error)
             }
-        }
-        if (interaction.options.getSubcommand() === 'ban_users') {
-            let guildData = await Guild.findOne({ _id: interaction.guild.id} )
-            const choice= interaction.options.getString('ban_users_option')
-            console.log(`Вибір `+choice)
-            console.log(guildData.logchannel)
-            if(choice==='true') {
-                try{
-                    console.log('Викликається!')
-                    await Guild.updateOne(
-                        { _id: interaction.guild.id },
-                        { $set: { blocking_enabled: true } }
-                    );
-                    await interaction.reply(`Успішно ввімкнено!`)
-                }catch(error) {
-                    console.log(error)
-                }
-            }
-            
         }
 
     
