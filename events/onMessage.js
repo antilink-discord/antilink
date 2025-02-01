ea63cdbf804e96ca69bcd17bbc4bc961b9956851
Максим <60219618+FelladoR@users.noreply.github.com>
2025-02-01 10:47:08 +0200

    fixed onMessage event

--- events/onMessage.js
+++ events/onMessage.js
@@ -4,12 +4,16 @@ const fs = require('fs');
 require('dotenv').config();
 const Guild = require('../Schemas/guildSchema')
 const User = require('../Schemas/userSchema')
+const { sendBanMessage, canBotBanMember} = require('../utils/sendDmMessages')
+const { guild_link_delete_log, guild_ban_log } = require('../utils/guildLogs')
+const { getTranslation } = require('../utils/helper')
+const { banLogs, linkLogs } = require('../utils/devLogs')
 module.exports = {
     name: Events.MessageCreate,
 
     async execute(message) {
         try{
-            const userData = User.findOne({ _id: message.author.id})
+            const guild = message.guild
             const isRole = await check_whitelist_and_owner(message)
             const user_id = message.author.id
             const channel_name = message.channel.name
@@ -18,43 +22,79 @@ module.exports = {
             const is_blocking_enabled = await check_blocking(message)
             console.log(`is_blocking_enabled: ${is_blocking_enabled}`)
             
-            if(isRole!=true && is_blocking_enabled==true && !message.author.bot) {
-                console.log('Контент' + message.content)
+            if(isRole!=true && is_blocking_enabled==true && !message.author.bot && message.author.id != message.guild.ownerId) {
+                const userData = await User.findOne({ _id: message.author.id})
+                if(!userData) {
+                    return
+                }else if(userData) {
+
+                
+                if(userData.warns >= 3) {
+                    const member = message.guild.members.cache.get(message.author.id)
+                    const warnsCount = userData.warns
+                    const botMember = message.guild.members.cache.get(message.client.user.id)
+                    const canBan = await canBotBanMember(botMember, member) // Перевірка, чи може бот заблокувати людину(наявність прав і позиція ролі)
+                    if(canBan) {
+                        try{
+                            
+                            const user = member.user
+                            
+                            await sendBanMessage(user, guild)
+                            await member.ban()
+                            await guild_ban_log(message, user_id, channel_name)
+                            await banLogs(message, user, guild, warnsCount)
+                            
+                        }catch(error) {
+                            console.log(console.log(error))
+                            try{ 
+                                await member.ban()
+                            }catch(error) {
+                                console.log(error)
+                            }
+                        }
+                    }else {
+                        console.log('[canBan]не можу заблокувати користувача')
+                        return
+                    }
+                }
+            }
+
                 const regex = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li|club)|discord(app)?\.com\/invite)\/\S+/i;
-                console.log(userData.warns)
                 const isMessageLink = regex.test(message.content) 
-                console.log(isMessageLink)
                 if(isMessageLink == true) {
                     
                     await User.updateOne(
-                        { _id: user_id },  // Пошук за id користувача
+                        { _id: user_id },  
                         {
-                          $inc: { warns: 1 },  // Збільшення попереджень
-                          $push: {  // Додавання нової причини в масив reasons
+                          $inc: { warns: 1 },  
+                          $push: { 
                             reasons: {
                               author_id: 'BOT',
-                              reason: "[Auto] links",
+                              reason: "[Auto]links",
                               proofs: null
                             }
                           }
                         },
-                        { upsert: true }  // Якщо не знайдено, створює новий документ
+                        { upsert: true }  
                       );
                     
                     const ExampleEmbed = new EmbedBuilder()
 
                     .setColor(0xE53935)
-                    .setTitle(`❌Посилання заблоковано`)
-                    .setDescription('На даній гільдії заборонено використовувати запрошувальні посилання')
-                
-                    
+                    .setTitle(await getTranslation(guild.id, "no_link_title"))
+                    .setDescription(await getTranslation(guild.id, "no_links_description"))
                 
                     try{
                         await message.delete().then(message => {
-                            message.channel.send({ content: `<@${message.author.id}>`,embeds: [ExampleEmbed]})
-                            send_to_guild_logschannel(message, user_id, channel_name )
+                            message.channel.send({ content: `<@${message.author.id}>`,embeds: [ExampleEmbed]}).then(message => {
+                                setTimeout(() => {
+                                    message.delete().catch(console.error);
+                                }, 10000);
+                            })
+                            guild_link_delete_log(message, user_id, channel_name )
                         })
-                    }catch(error) {
+                        await linkLogs(message, user, guild, warnsCount)
+                    }catch(error) { 
                         console.log('Не вдалось видалити повідомлення')
                     }
                 }
@@ -69,24 +109,23 @@ module.exports = {
 }
 async function check_whitelist_and_owner(message) {
     try {
-        const guildData = await Guild.findOne({ _id: message.guild.id }); // Отримуємо дані гільдії
-        const whitelist_data = guildData ? guildData.whitelist : []; // whitelist з бази даних, перевіряємо на null
-        const member = message.member; // Отримуємо учасника, що надіслав повідомлення
+        const guildData = await Guild.findOne({ _id: message.guild.id });
+        const whitelist_data = guildData ? guildData.whitelist : []; 
+        const member = message.member; 
 
         if (!member) {
             console.log('Не вдалося отримати учасника!');
-            return; // Якщо member відсутній, припиняємо виконання
+            return; 
         }
 
-        // Виведення ролей користувача
-        const memberRoles = member.roles.cache; // Використовуємо cache без fetch()
+        const memberRoles = member.roles.cache
 
         console.log('Ролі користувача:');
         memberRoles.forEach(role => {
-            console.log(`- ${role.name} (ID: ${role.id})`); // Виведемо кожну роль
+            console.log(`- ${role.name} (ID: ${role.id})`)
         });
 
-        // Перевірка, чи є роль користувача в whitelist
+ 
         const hasWhitelistedRole = memberRoles.some(role => whitelist_data.includes(role.id));
 
         if (hasWhitelistedRole) {
@@ -103,7 +142,7 @@ async function check_whitelist_and_owner(message) {
 }
 async function check_blocking(message) {
     try {
-        const guildData = await Guild.findOne({ _id: message.guild.id }); // Отримуємо дані гільдії
+        const guildData = await Guild.findOne({ _id: message.guild.id }); 
         const blockingData = guildData ? guildData.blocking_enabled: false;
         if(blockingData==true) {
             console.log('Блокування ввімкнено на сервері')
@@ -117,22 +156,3 @@ async function check_blocking(message) {
     }
 }
 
-async function send_to_guild_logschannel(message, user_id, channel_name) {
-    guildData = await Guild.findOne({ _id: message.guild.id})
-    guild_logchannel = guildData.logchannel
-    console.log(guild_logchannel)
-    if(guild_logchannel) {
-    const webhook = new WebhookClient({ url: guild_logchannel})
-    const log_embed = new EmbedBuilder()
-        .setTitle('Блокування запрошення')
-        .setColor(0x5e66ff)
-        .addFields(
-            { name: `Користувач`, value: `<@${user_id}> || \`\`${user_id}\`\`` , inline: true },
-            { name: `Канал`, value: `${channel_name}`, inline: true },
-            { name: 'Повідомлення', value: `\`\`${message}\`\``, inline: false },
-            // { name: 'Розробник', value: `Maksym_Tyvoniuk`, inline: false }
-        )
-    
-    await webhook.send({ embeds: [log_embed]})
-    }
-}
