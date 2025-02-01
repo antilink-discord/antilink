7b0fb4d86e2ce07b508e35c33c5e4f4b000a0e92
Максим <60219618+FelladoR@users.noreply.github.com>
2025-02-01 21:01:16 +0200

    deleted comments

--- events/onReady.js
+++ events/onReady.js
@@ -15,17 +15,14 @@ module.exports = {
         client.commands = new Collection()
         const foldersPath = path.join(__dirname, '..', 'commands');
 
-        // Рекурсивна функція для збору всіх команд
         async function loadCommands(folderPath) {
             const entries = fs.readdirSync(folderPath, { withFileTypes: true });
             for (const entry of entries) {
                 const fullPath = path.join(folderPath, entry.name);
 
-                // Якщо це папка, запускаємо рекурсію
                 if (entry.isDirectory()) {
                     await loadCommands(fullPath);
                 } else if (entry.isFile() && entry.name.endsWith('.js')) {
-                    // Якщо це файл команди, підключаємо його
                     const command = require(fullPath);
                     if ('data' in command && 'execute' in command) {
                         client.commands.set(command.data.name, command);
@@ -37,12 +34,10 @@ module.exports = {
             }
         };
 
-        // Запускаємо рекурсивний обхід команд
         await loadCommands(foldersPath);
 
         console.log('Усі команди успішно завантажені!');
 
-        // Реєстрація команд на сервері
         const commands = client.commands.map(command => command.data.toJSON());
 
         const rest = new REST({ version: '10' }).setToken(token);
@@ -50,9 +45,8 @@ module.exports = {
         try {
             console.log('Реєстрація команд...');
 
-            // Реєстрація локальних команд на гільдії
             await rest.put(
-                Routes.applicationCommands(clientId), // <- замість applicationGuildCommands
+                Routes.applicationCommands(clientId), 
                 { body: commands },
             );
             console.log('Локальні команди успішно зареєстровані!');
