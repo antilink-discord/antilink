a2b215db503c6194f4420b4cf1225e13cc728aff
Максим <60219618+FelladoR@users.noreply.github.com>
2025-02-01 10:42:52 +0200

    upgraded onGuildJoin

--- events/onGuildJoin.js
+++ events/onGuildJoin.js
@@ -3,60 +3,38 @@ const path = require('path');
 const fs = require('fs');
 require('dotenv').config();
 const Guild = require('../Schemas/guildSchema')
-const token = process.env.TOKEN;
-const clientId = process.env.CLIENT_ID; // Ваш client ID
-const guildId = process.env.GUILD_ID; // ID сервера (guild) для реєстрації команд
-
+const { sendJoinLogs } = require('../utils/devLogs')
 module.exports = {
     name: Events.GuildCreate,
     once: false,
     async execute(guild) {
-        const client = guild.client
-        let guildData = await Guild.findOne({ _id: guild.id})
-        if(guildData) {
-            console.log('Дані гільдії вже присутні в базі даних')
-            await sendJoinLogs(guild, clien)
-            return
-        } else {
-            guildData = new Guild({ _id: guild.id})
-            console.log('Не знайшов гільдію в базі даних, додаю...')
-            await guildData.save()
-            await sendJoinLogs(guild, client)
-        }
-    }
-}
-async function sendJoinLogs(guild, client) {
-    try {
-        const supportserverid = process.env.SUPPORT_SERVER_ID;
-        const devchannellogs = process.env.DEV_LOGS_ID;
-
-        if (!client || !client.guilds) {
-            console.error('Об\'єкт client не переданий або client.guilds не існує!');
-            return;
-        }
-
-        const support_server = await client.guilds.fetch(supportserverid);
-        if (!support_server) {
-            console.error(`Не вдалося знайти сервер із ID ${supportserverid}`);
-            return;
-        }
-
-        const devlogchannel = await support_server.channels.fetch(devchannellogs);
-        if (!devlogchannel) {
-            console.error(`Не вдалося знайти канал із ID ${devchannellogs}`);
-            return;
-        }
-
-        const ExampleEmbed = new EmbedBuilder()
-            .setColor(0x427bff)
-            .setTitle('Вхід на сервер')
-            .addFields(
-                { name: 'Гільдія', value: `${guild.name} | \`\`${guild.id}\`\``, inline: true }
-            )
-            .setTimestamp();
-
-        await devlogchannel.send({ embeds: [ExampleEmbed] });
-    } catch (error) {
-        console.error('Помилка у sendDevLogs:', error);
+        try{
+            console.log(guild)
+            
+            const client = guild.client
+            let guildData = await Guild.findOne({ _id: guild.id})
+            
+            if(guildData) {
+                console.log('Дані гільдії вже присутні в базі даних')
+                await sendJoinLogs(guild, client)
+                return
+            } 
+                if(guild.preferredLocale == 'uk') {
+                    if(guildData){
+                        return console.log('Гільдія вже в базі')
+                    } 
+                    guildData = new Guild({ _id: guild.id, language: 'uk'})
+                    await guildData.save()
+                    await sendJoinLogs(guild, client)
+                    return
+                }
+                
+                guildData = new Guild({ _id: guild.id})
+                console.log('Не знайшов гільдію в базі даних, додаю...')
+                await guildData.save()
+                await sendJoinLogs(guild, client)
+        }catch(error) {
+            console.log(error)
+        }   
     }
 }
