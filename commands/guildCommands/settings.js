288b10b789217c8f77133a68f7019c05e66a00a3
Максим <60219618+FelladoR@users.noreply.github.com>
2025-02-01 10:49:50 +0200

    upgraded settings

--- commands/guildCommands/settings.js
+++ commands/guildCommands/settings.js
@@ -1,118 +1,41 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { EmbedBuilder, MessageFlags, Message } = require('discord.js');
-const moment = require("moment");
-require("moment-duration-format");
-const { version } = require('discord.js');
-const Guild = require('../../Schemas/guildSchema')
-const { getTranslation } = require('../../utils/helper')
+const { EmbedBuilder, MessageFlags } = require('discord.js');
+const Guild = require('../../Schemas/guildSchema');
+const { getTranslation } = require('../../utils/helper');
+const { settingsHandler } = require('../../utils/settingsHandler');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('settings')
         .setDescription('Відкриває налаштування вашої гільдії'),
 
-    // Визначення execute з параметром client
     async execute(interaction) {
-        try{
-            let guildData = await Guild.findOne({_id: interaction.guild.id})
-            // Перевіряємо, чи доступне uptime через переданий client
-            if(interaction.guild.ownerId == interaction.member.id) {
-                let role_names
-                const support_server = await interaction.client.guilds.cache.get(process.env.SUPPORT_SERVER_ID)
-                role_names = await format_whitelist(interaction)
-                console.log(`Передано список ролей ` + role_names)
-                if(!support_server && support_server==null) {
-                    console.log('Не вдалось знайти сервер підтримки')
-                }
-                let userblocking;
-                if(!guildData) {
-                    console.log('Не знайдено налаштувань')
-                    guildData = new Guild({ _id: interaction.guild.id})
+        try {
+            let guildData = await Guild.findOne({ _id: interaction.guild.id });
 
-                }
-                console.log(guildData)
-                const emoji_park = await get_emojis_for_message(support_server)
-                
-                if(guildData.userblocking==true) {
-                    userblocking = await getTranslation(interaction.guild.id, "settings_enabled")
-                }
-                else if(guildData.userblocking==false || !guildData.userblocking) {
-                    userblocking = await getTranslation(interaction.guild.id, "settings_disabled")
-                } 
-                if(!guildData.logchannel && guildData.logchannel == null){guildData.logchannel=`${await getTranslation(interaction.guild.id, "settings_nodata")}`}
-                if(!role_names ){guildData.whitelist=`${await getTranslation(interaction.guild.id, "settings_nodata")}`}
-                if(!guildData.userblocking && guildData.userblocking == null){guildData.userblocking=`${await getTranslation(interaction.guild.id, "settings_disabled")}`}
-                // Створюємо ембед
+            if (interaction.guild.ownerId === interaction.member.id) {
+                const { webhook_name, webhook_channel, userblocking, role_names, emoji_pack } = await settingsHandler(interaction);
+
                 const ExampleEmbed = new EmbedBuilder()
+                    .setColor(0x5e66ff)
+                    .setTitle(`${emoji_pack.settings_emoji}${await getTranslation(interaction.guild.id, "settings_title")}`)
+                    .setDescription(await getTranslation(interaction.guild.id, "settings_description"))
+                    .addFields(
+                        { name: `${emoji_pack.logs_channel_emoji}${await getTranslation(interaction.guild.id, "settings_logchannel")}`, value: `${webhook_name} | ${webhook_channel}` || `${await getTranslation(interaction.guild.id, "settings_didnt_setup")}`, inline: true },
+                        { name: `${emoji_pack.whitelist_emoji}${await getTranslation(interaction.guild.id, "settings_whitelist")}`, value: role_names.join(', ') || `${await getTranslation(interaction.guild.id, "settings_didnt_setup")}`, inline: true },
+                        { name: `${await getTranslation(interaction.guild.id, "settings_blocking")}`, value: userblocking, inline: false },
+                    )
+                    .setFooter({ text: await getTranslation(interaction.guild.id, "settings_footer") });
 
-                .setColor(0x5e66ff)
-                .setTitle(`${emoji_park.settings_emoji}${await getTranslation(interaction.guild.id, "settings_title")}`)
-                .setDescription(await getTranslation(interaction.guild.id, "settings_description"))
-                .addFields(
-                    { name: `${emoji_park.logs_channel_emoji}${await getTranslation(interaction.guild.id, "settings_logchannel")}`, value: `<#${guildData.logchannel}>` || `${await getTranslation(interaction.guild.id, "settings_didnt_setup")}`, inline: true },
-                    { name: `${emoji_park.whitelist_emoji}${await getTranslation(interaction.guild.id, "settings_whitelist")}`, value: `${await getTranslation(interaction.guild.id, "settings_didnt_setup")}` || `${await getTranslation(interaction.guild.id, "settings_didnt_setup")}`, inline: true },
-                    { name: `${await getTranslation(interaction.guild.id, "settings_blocking")}`, value: userblocking || `${await getTranslation(interaction.guild.id, 'settings_disabled')}`, inline: false },
-                    // { name: 'Розробник', value: `Maksym_Tyvoniuk`, inline: false }
-                )
-                .setFooter({text: await getTranslation(interaction.guild.id, "settings_footer")})
-
-        // Якщо вже є відповідь, редагуємо її
-
-                await interaction.reply({ embeds: [ExampleEmbed], flags: MessageFlags.Ephemeral});
-        
+                await interaction.reply({ embeds: [ExampleEmbed], flags: MessageFlags.Ephemeral });
             } else {
-                await interaction.reply({ content: await getTranslation(interaction.guild.id, "no_perms"), flags: MessageFlags.Ephemeral})
-                console.log('Немає прав власника серверу, неможна використовувати команду')
-                return
+                await interaction.reply({ content: await getTranslation(interaction.guild.id, "no_perms"), flags: MessageFlags.Ephemeral });
+                console.log('Немає прав власника серверу, неможна використовувати команду');
+                return;
             }
-        }catch(error) {
-            console.log('Помилка settings.js: '+error)
+        } catch (error) {
+            console.log('Помилка settings.js: ' + error);
         }
-        
-        
     },
 };
 
-async function get_emojis_for_message(support_server) {
-    try{
-    return{
-        settings_emoji: await support_server.emojis.cache.get('1266082934872604745'),
-        logs_channel_emoji: await support_server.emojis.cache.get('1266073334030926008'),
-        whitelist_emoji: await support_server.emojis.cache.get('1266073332152008704'),
-    }
-    }catch(error) {
-        console.log('Не вдалось отримати емодзі get_emoji_for_message: '+ error)
-        
-    }
-}
-
-async function format_whitelist(interaction) {
-    try {
-        const GuildData = await Guild.findOne({ _id: interaction.guild.id });
-        if (!GuildData || !GuildData.whitelist || GuildData.whitelist.length === 0) {
-            console.log('Бачу, що немає ролей');
-            return []; // Повертаємо порожній масив, якщо немає ролей
-        }
-
-        const rolesId = GuildData.whitelist;
-        let role_mentions = [];
-        console.log(`Айді ролей: ${rolesId}`);
-
-        rolesId.forEach(roleId => {
-            const role = interaction.guild.roles.cache.get(roleId);
-            if (role) {
-                console.log('Роль знайдено: ' + role.name);
-                // Додаємо згадку про роль до списку
-                role_mentions.push(role.toString()); // Згадка про роль
-            } else {
-                console.log('Роль з ID ' + roleId + ' не знайдена!');
-            }
-        });
-
-        return role_mentions; // Повертаємо масив згадок про ролі
-    } catch (error) {
-        console.error('Помилка у форматуванні білого списку:', error);
-        return []; // Повертаємо порожній масив у випадку помилки
-    }
-
-
-}
--- events/onInteractionCreate.js
+++ events/onInteractionCreate.js
@@ -1,32 +1,40 @@
 const { Events, MessageFlags, EmbedBuilder, Embed, ButtonBuilder, ButtonStyle, ActionRowBuilder, PermissionOverwriteManager, PermissionOverwrites, PermissionFlagsBits, ModalBuilder, TextInputBuilder, TextInputStyle, flatten } = require('discord.js');
 require('dotenv').config();
+const { interactionLogs } = require('../utils/devLogs')
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (interaction.isChatInputCommand()) {
-
-		const command = interaction.client.commands.get(interaction.commandName);
-
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
+		if (interaction.isAutocomplete()) {
+			try{
+				const command = interaction.client.commands.get(interaction.commandName);
+				await command.autocomplete(interaction);
+				return; 
+			}catch(error) {
+				console.log(error)
+			}
 		}
+		
+		if (interaction.isChatInputCommand()) {	
+			if (!interaction.inGuild() || !interaction.isCommand()) return;
+			const command = interaction.client.commands.get(interaction.commandName);
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+			
+			try {
 
-		try {
-			// if(interaction.user.id !="558945911980556288" && interaction.user.id !="614784992362496020") {
-			// 	console.log('Вимкнено використання команд для всіх(events/onInteractionCreate.js)')
-			// 	interaction.reply({content: 'Команда вимкнена.', flags: MessageFlags.Ephemeral})
-			// 	return
-			// }
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+				await command.execute(interaction);
+	
+			} catch (error) {
+				console.error(error);
+				if (interaction.replied || interaction.deferred) {
+					await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+				} else {
+					await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+				}
 			}
 		}
 	}
-}
-}
+	}
+	
