2486d0739f785a27728ffa796bfa07e5d1bc690a
Максим <maxum2607@gmail.com>
2025-05-01 11:21:14 +0200

    formated code with prettier

--- commands/devcommands/shards.js
+++ commands/devcommands/shards.js
@@ -1,35 +1,35 @@
-import "dotenv/config";
+import 'dotenv/config';
 import { SlashCommandBuilder } from "discord.js";
-import { shardManager } from "../../shardManager.js"; // Тепер імпортуємо іменований експорт
+import { shardManager } from '../../shardManager.js'; 
 
 export const data = new SlashCommandBuilder()
-  .setName("shards")
-  .setDescription("Показує статус усіх шардів");
+    .setName('shards')
+    .setDescription('Показує статус усіх шардів');
 
 export async function execute(interaction) {
-  if (interaction.user.id !== "558945911980556288") {
-    return await interaction.reply("У вас немає доступу до цієї команди.");
-  }
-
-  try {
-    if (!shardManager) {
-      return await interaction.reply("Менеджер шардів не ініціалізований.");
+    if (interaction.user.id !== '558945911980556288') {
+        return await interaction.reply('У вас немає доступу до цієї команди.');
     }
 
-    const statusMessages = [];
+    try {
+        if (!shardManager) {
+            return await interaction.reply('Менеджер шардів не ініціалізований.');
+        }
 
-    for (const shard of shardManager.shards.values()) {
-      const status = shard.ready ? "🟢 Онлайн" : "🔴 Оффлайн";
-      const guilds = await shard.fetchClientValue("guilds.cache.size");
-      statusMessages.push(`Шард ${shard.id}: ${status} | Серверів: ${guilds}`);
+        const statusMessages = [];
+        
+        for (const shard of shardManager.shards.values()) {
+            const status = shard.ready ? '🟢 Онлайн' : '🔴 Оффлайн';
+            const guilds = await shard.fetchClientValue('guilds.cache.size');
+            statusMessages.push(`Шард ${shard.id}: ${status} | Серверів: ${guilds}`);
+        }
+
+        await interaction.reply({
+            content: `**Статус шардів:**\n${statusMessages.join('\n')}`,
+            ephemeral: true
+        });
+    } catch (error) {
+        console.error('Помилка команди shards:', error);
+        await interaction.reply('Сталася помилка при отриманні статусу шардів.');
     }
-
-    await interaction.reply({
-      content: `**Статус шардів:**\n${statusMessages.join("\n")}`,
-      ephemeral: true,
-    });
-  } catch (error) {
-    console.error("Помилка команди shards:", error);
-    await interaction.reply("Сталася помилка при отриманні статусу шардів.");
-  }
 }
--- commands/devcommands/warn.js
+++ commands/devcommands/warn.js
@@ -1,87 +1,84 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { EmbedBuilder } from "discord.js";
-import Warning from "../../Schemas/userSchema.js"; // Шлях до схеми попереджень
-import "dotenv/config";
-import { delete_cache } from "../../utils/userWarningsCaching.js";
+import { SlashCommandBuilder } from '@discordjs/builders';
+import Warning from '../../Schemas/userSchema.js'; 
+import 'dotenv/config'
+import { delete_cache } from '../../utils/userWarningsCaching.js';
 
 const SUPPORT_SERVER_ID = process.env.SUPPORT_SERVER_ID;
 const MOD_ROLE_ID = process.env.MOD_ROLE_ID;
 const DEV_ROLE_ID = process.env.DEV_ROLE_ID;
 
-import Logger from "../../utils/logs.js";
-const lg = new Logger({ prefix: "Bot" });
 
-export const data = new SlashCommandBuilder()
-  .setName("warn")
-  .setDescription("Команда для видачі попередження користувачу")
-  .addStringOption((option) =>
-    option
-      .setName("user_id")
-      .setDescription("Користувач якому видається попередження")
-      .setRequired(true),
-  )
-  .addStringOption((option) =>
-    option
-      .setName("reason")
-      .setDescription("Причина попередження")
-      .setRequired(true)
-      .addChoices(
-        { name: "⚠ Реклама серверів", value: "spam links" },
-        { name: "🤖 Краш бот", value: "crash bot" },
-        { name: "🧨 Краш серверів", value: "crashing guild" },
-        {
-          name: "🛠 Розробка/реклама краш софту",
-          value: "crash bot/crash soft develop",
-        },
-        { name: "🧌 Русня", value: "russian" },
-        { name: "Рейд серверів", value: "raider" },
-      ),
-  )
-  .addStringOption((option) =>
-    option
-      .setName("proofs")
-      .setDescription("Докази порушення")
-      .setRequired(true),
-  );
+import Logger from '../../utils/logs.js';
+const lg = new Logger({ prefix: 'Bot' });
 
-export async function execute(interaction) {
-  const role = interaction.guild.roles.cache.get(MOD_ROLE_ID);
-  if (interaction.guild.id == SUPPORT_SERVER_ID) {
-    if (
-      interaction.member.roles.cache.get(MOD_ROLE_ID) ||
-      interaction.member.roles.cache.get(DEV_ROLE_ID)
-    ) {
-      try {
-        const target = interaction.options.getString("user_id");
+    export const data = new SlashCommandBuilder()
+		.setName('warn')
+		.setDescription('Команда для видачі попередження користувачу')
+		.addStringOption(option =>
+			option.setName('user_id')
+				.setDescription('Користувач якому видається попередження')
+				.setRequired(true),
 
-        let targetWarns = await Warning.findOne({ _id: target });
-        if (!targetWarns) {
-          targetWarns = new Warning({ _id: target, warns: 0 });
-        }
+		)
+		.addStringOption(option =>
+			option.setName('reason')
+				.setDescription('Причина попередження')
+				.setRequired(true)
+				.addChoices(
+					{ name: '⚠ Реклама серверів', value: 'spam links' },
+					{ name: '🤖 Краш бот', value: 'crash bot' },
+					{ name: '🧨 Краш серверів', value: 'crashing guild' },
+					{ name: '🛠 Розробка/реклама краш софту', value: 'crash bot/crash soft develop' },
+					{ name: '🧌 Русня', value: 'russian' },
+					{ name: 'Рейд серверів', value: 'raider' },
+				),
+		)
+		.addStringOption(option =>
+			option.setName('proofs')
+				.setDescription('Докази порушення')
+				.setRequired(true),
+		)
 
-        const date = new Date();
-        const formatted_date = date.toLocaleString();
-        const reason = interaction.options.getString("reason");
 
-        targetWarns.warns += 1;
-        targetWarns.reasons.push({
-          author_id: interaction.user.id,
-          reason: reason,
-          proofs: interaction.options.getString("proofs"),
-          timestamp: formatted_date,
-        });
+	export async function execute(interaction) {
 
-        await targetWarns.save();
-        const user_id = target;
-        delete_cache(user_id);
-        await interaction.reply(
-          `Попередження видано успішно! Загальна кількість попереджень: ${targetWarns.warns}`,
-        );
-      } catch (error) {
-        lg.error(error);
-      }
+		if (interaction.guild.id == SUPPORT_SERVER_ID) {
+
+			if (interaction.member.roles.cache.get(MOD_ROLE_ID) || interaction.member.roles.cache.get(DEV_ROLE_ID)) {
+				try {
+
+					const target = interaction.options.getString('user_id');
+
+
+					let targetWarns = await Warning.findOne({ _id: target });
+					if (!targetWarns) {
+						targetWarns = new Warning({ _id: target, warns: 0 });
+					}
+
+					const date = new Date();
+					const formatted_date = date.toLocaleString();
+					const reason = interaction.options.getString('reason');
+
+					targetWarns.warns += 1;
+					targetWarns.reasons.push({
+						author_id: interaction.user.id,
+						reason: reason,
+						proofs: interaction.options.getString('proofs'),
+						timestamp: formatted_date,
+					});
+
+					await targetWarns.save();
+					const user_id = target;
+					delete_cache(user_id);
+					await interaction.reply(`Попередження видано успішно! Загальна кількість попереджень: ${targetWarns.warns}`);
+				}
+				catch (error) {
+					lg.error(error);
+				}
+			}
+		}
+		else {
+			return;
+		}
+
     }
-  } else {
-    return;
-  }
-}
