4efba68c3b4c000405be871b2c8ccc809a6930f8
Maksym_Tyvoniuk <tivonukmaksim@gmail.com>
2025-05-22 14:22:37 +0200

    minor bug fix

--- events/onGuildMemberAdd.js
+++ events/onGuildMemberAdd.js
@@ -12,7 +12,7 @@ export default {
     lg.info("Виклик івенту GuildMemberAdd");
     try {
       const guildData = await Guild.findOne({ _id: member.guild.id });
-      if (guildData.verificationSystem?.isEnabled === true){
+      if (guildData?.verificationSystem?.isEnabled === true){
         const role = await member.guild.roles.fetch(guildData.verificationSystem.unvefivedRoleID);
         if (role) {
           await member.roles.add(role).catch(error => {
